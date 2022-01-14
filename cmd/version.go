01a5da553cfca658e0dd767375b134f85011f26c
alpha <alpha@163.com>
2022-01-14 19:32:40 +0800

    update template

--- cmd/version.go
+++ cmd/version.go
@@ -15,6 +15,6 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of goinit",
 	Long:  `All software has versions. This is goinit's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("goinit version v0.4.7")
+		fmt.Println("goinit version v0.4.8")
 	},
 }
